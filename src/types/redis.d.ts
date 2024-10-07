dc5927bfad94b5be8d27458883ceeb7096f6e9b4
Marc España Quinquilla <meq.1515@gmail.com>
2024-10-07 19:22:26 +0200

    I have to add authorId as a propety

--- src/types/redis.d.ts
+++ src/types/redis.d.ts
@@ -1,10 +1,11 @@
-import { Vote } from '@prisma/client'
+import { Vote } from "@prisma/client";
 
 export type CachedPost = {
-  id: string
-  title: string
-  authorUsername: string
-  content: string
-  currentVote: Vote['type'] | null
-  createdAt: Date
-}
+  id: string;
+  title: string;
+  authorId: string; // Añadir `authorId` a CachedPost
+  authorUsername: string;
+  content: string;
+  currentVote: Vote["type"] | null;
+  createdAt: Date;
+};
