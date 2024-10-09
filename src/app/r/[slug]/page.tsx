e3de490b7788faea0d13bae11a216f9ed30ac1bb
Marc España Quinquilla <meq.1515@gmail.com>
2024-10-09 18:53:21 +0200

    When we call the name of the subreddits we need to transform the word with the spaces again for a better UX

--- src/app/r/[slug]/page.tsx
+++ src/app/r/[slug]/page.tsx
@@ -1,20 +1,28 @@
-import MiniCreatePost from '@/components/MiniCreatePost'
-import PostFeed from '@/components/PostFeed'
-import { INFINITE_SCROLL_PAGINATION_RESULTS } from '@/config'
-import { getAuthSession } from '@/lib/auth'
-import { db } from '@/lib/db'
-import { notFound } from 'next/navigation'
+import MiniCreatePost from "@/components/MiniCreatePost";
+import PostFeed from "@/components/PostFeed";
+import { INFINITE_SCROLL_PAGINATION_RESULTS } from "@/config";
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { notFound } from "next/navigation";
 
 interface PageProps {
   params: {
-    slug: string
-  }
+    slug: string;
+  };
+}
+
+// Función para convertir los guiones en espacios y capitalizar la primera letra de cada palabra
+function formatSubredditName(name: string) {
+  return name
+    .split("-") // Dividir el nombre en palabras separadas por guiones
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1)) // Capitalizar la primera letra de cada palabra
+    .join(" "); // Unir las palabras con un espacio
 }
 
 const page = async ({ params }: PageProps) => {
-  const { slug } = params
+  const { slug } = params;
 
-  const session = await getAuthSession()
+  const session = await getAuthSession();
 
   const subreddit = await db.subreddit.findFirst({
     where: { name: slug },
@@ -27,24 +35,27 @@ const page = async ({ params }: PageProps) => {
           subreddit: true,
         },
         orderBy: {
-          createdAt: 'desc'
+          createdAt: "desc",
         },
         take: INFINITE_SCROLL_PAGINATION_RESULTS,
       },
     },
-  })
+  });
 
-  if (!subreddit) return notFound()
+  if (!subreddit) return notFound();
+
+  // Aplicamos el formateo al nombre del subreddit
+  const formattedSubredditName = formatSubredditName(subreddit.name);
 
   return (
     <>
-      <h1 className='font-bold text-3xl md:text-4xl h-14'>
-        r/{subreddit.name}
+      <h1 className="font-bold text-3xl md:text-4xl h-14">
+        r/{formattedSubredditName} {/* Mostramos el nombre formateado */}
       </h1>
       <MiniCreatePost session={session} />
       <PostFeed initialPosts={subreddit.posts} subredditName={subreddit.name} />
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
