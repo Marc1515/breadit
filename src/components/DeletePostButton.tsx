19773692de0d66b7048537b9f5299faccae2003e
Marc España Quinquilla <meq.1515@gmail.com>
2024-10-10 15:58:58 +0200

    I have added a modal to control the delete action

--- src/components/DeletePostButton.tsx
+++ src/components/DeletePostButton.tsx
@@ -4,6 +4,16 @@ import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Button } from "./ui/Button";
+import { Trash } from "lucide-react";
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+} from "./ui/Dialog";
 
 interface DeletePostButtonProps {
   postId?: string;
@@ -12,12 +22,17 @@ interface DeletePostButtonProps {
 export const DeletePostButton = ({ postId }: DeletePostButtonProps) => {
   const router = useRouter();
   const [isDeleting, setIsDeleting] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
 
   const deletePost = async () => {
     setIsDeleting(true);
     try {
       await axios.delete(`/api/subreddit/post/delete/${postId}`);
-      alert("Post deleted succesfully");
+      closeModal();
+      alert("Post deleted successfully");
       router.push("/");
     } catch (error) {
       console.error("Error deleting post:", error);
@@ -28,14 +43,41 @@ export const DeletePostButton = ({ postId }: DeletePostButtonProps) => {
   };
 
   return (
-    <Button
-      className={`text-red-600 hover:text-red-800 ${
-        isDeleting ? "opacity-50 cursor-not-allowed" : ""
-      }`}
-      onClick={deletePost}
-      disabled={isDeleting}
-    >
-      DeletePostButton
-    </Button>
+    <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+      <DialogTrigger asChild>
+        <Button
+          className={`hover:text-red-800 ${
+            isDeleting ? "opacity-50 cursor-not-allowed" : ""
+          }`}
+          disabled={isDeleting}
+          variant={"ghost"}
+          onClick={openModal}
+        >
+          <Trash className="w-4 h-4" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Delete Post</DialogTitle>
+          <DialogDescription>
+            Are you sure you want to delete this post? This action cannot be
+            undone.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button variant="ghost" onClick={closeModal}>
+            Cancel
+          </Button>
+          <Button
+            className="bg-red-500"
+            variant="destructive"
+            onClick={deletePost}
+            disabled={isDeleting}
+          >
+            {isDeleting ? "Deleting..." : "Delete"}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 };
