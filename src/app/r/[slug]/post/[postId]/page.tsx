e658049aa3e0a3f8feca32aad7121054857164f9
Marc España Quinquilla <meq.1515@gmail.com>
2024-10-10 15:57:23 +0200

    Just fixind a responsive div

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -68,15 +68,18 @@ const SubRedditPostPage = async ({ params }: SubRedditPostPageProps) => {
         </Suspense>
 
         <div className="sm:w-0 w-full flex-1 bg-white p-4 rounded-sm">
-          <p className="max-h-40 mt-1 truncate text-xs text-gray-500">
-            Posted by u/{post?.author.username ?? cachedPost.authorUsername}{" "}
-            {formatTimeToNow(new Date(post?.createdAt ?? cachedPost.createdAt))}
-          </p>
+          <div className="flex justify-between">
+            <p className="max-h-40 mt-1 truncate text-xs text-gray-500">
+              Posted by u/{post?.author.username ?? cachedPost.authorUsername}{" "}
+              {formatTimeToNow(
+                new Date(post?.createdAt ?? cachedPost.createdAt)
+              )}
+            </p>
+            {isAuthor && <DeletePostButton postId={post?.id} />}
+          </div>
           <h1 className="text-xl font-semibold py-2 leading-6 text-gray-900">
             {post?.title ?? cachedPost.title}
           </h1>
-
-          {isAuthor && <DeletePostButton postId={post?.id} />}
 
           <EditorOutput content={post?.content ?? cachedPost.content} />
           <Suspense
